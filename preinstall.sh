19cf36dad3676715a1e9fa6a9b12e3ca5bdf4ddf
Shaohua Li <shaohua.li@inf.ethz.ch>
2022-10-24 22:53:53 +0200

    add new package requirement

--- preinstall.sh
+++ preinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 add-apt-repository -y ppa:ubuntu-toolchain-r/test
-apt-get update && apt-get -y install gcc-11 g++-11 libssl-dev gcc-11-plugin-dev libc++-dev libc++abi-dev
+apt-get update && apt-get -y install gcc-11 g++-11 libssl-dev gcc-11-plugin-dev libc++-dev libc++abi-dev jq
 
 wget https://apt.llvm.org/llvm.sh
 chmod +x ./llvm.sh
